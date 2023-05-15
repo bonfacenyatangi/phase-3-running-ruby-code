9019056952248e3c59d7975ebcd2fc6a727ef3c9
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-05-15 12:24:51 +0300

    ruby printing

--- app.rb
+++ app.rb
@@ -13,11 +13,16 @@
 # end
 # # 1 + 1
 # puts add + add
+# In Ruby, all methods have a return value. 
+# def add num1, num2
+#     p num1
+#     p num2
+#     num1 + num2
+# end
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# p add(1,3) + add(2,2)
 
-puts add(1,3) + add(2,2)
+# pp [1, 2, 3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
